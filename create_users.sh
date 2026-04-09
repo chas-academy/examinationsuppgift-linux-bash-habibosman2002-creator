a04d2295c1dac6d1ba6871a131b56b167f679e9d
habibosman2002-creator <habibosman2002@gmail.com>
2026-04-10 00:45:23 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+# Kontrollera root
 if [ "$EUID" -ne 0 ]; then
-  echo "Scriptet måste köras som root!"
-  exit 1
+    echo "Scriptet måste köras som root!"
+    exit 1
 fi
 
+# Loop genom alla användare
 for user in "$@"
 do
-  useradd -m "$user"
+    # Skapa användare
+    useradd -m "$user"
 
-  mkdir -p /home/$user/Documents
-  mkdir -p /home/$user/Downloads
-  mkdir -p /home/$user/Work
+    # Skapa grupp och lägg till användare
+    groupadd "$user"
+    usermod -aG "$user" "$user"
 
-  chown -R $user:$user /home/$user
-  chmod -R 700 /home/$user
+    # Skapa mappar
+    mkdir -p /home/$user/Documents
+    mkdir -p /home/$user/Downloads
+    mkdir -p /home/$user/Work
 
-  echo "Välkommen $user" > /home/$user/welcome.txt
+    # Sätt rättigheter
+    chown -R $user:$user /home/$user
+    chmod -R 700 /home/$user
 
-  for other in $(ls /home)
-  do
-    if [ "$other" != "$user" ]; then
-      echo "$other" >> /home/$user/welcome.txt
-    fi
-  done
+    # Skapa välkomstfil
+    echo "Välkommen $user" > /home/$user/welcome.txt
+
+    # Lista andra användare
+    for other in $(ls /home)
+    do
+        if [ "$other" != "$user" ]; then
+            echo "$other" >> /home/$user/welcome.txt
+        fi
+    done
+
 done
