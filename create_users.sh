c0e04bd9c9a296ce4df460558bb9a7dc6c8b9ba2
habibosman2002-creator <habibosman2002@gmail.com>
2026-04-10 00:38:24 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,20 +1,27 @@
-#!/bin/bash 
-if [ "$EUID" -ne 0 ]; then 
-echo "Scriptet måste köras som root!" 
-exit 1 
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Scriptet måste köras som root!"
+  exit 1
 fi
+
 for user in "$@"
 do
-useradd -m "$user" 
-mkdir -p /home/$user/Documents
-mkdir -p /home/$user/Downloads 
-mkdir -p /home/$user/Work
-chown -R $user:$user /home/$user 
-chmod -R 700 /home/$user
-echo "Välkommen $user"> /home/$user/welcome.txt
-for other in $(1s /home); do
-if [ "$other" != "$user" ]; then 
-echo "$other" >> /home/$user/welcome.txt 
-fi 
-done 
-done 
+  useradd -m "$user"
+
+  mkdir -p /home/$user/Documents
+  mkdir -p /home/$user/Downloads
+  mkdir -p /home/$user/Work
+
+  chown -R $user:$user /home/$user
+  chmod -R 700 /home/$user
+
+  echo "Välkommen $user" > /home/$user/welcome.txt
+
+  for other in $(ls /home)
+  do
+    if [ "$other" != "$user" ]; then
+      echo "$other" >> /home/$user/welcome.txt
+    fi
+  done
+done
